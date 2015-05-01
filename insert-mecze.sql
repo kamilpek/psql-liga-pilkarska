861ddf3dc55163cf053c7b328aeea71b71a90646
Kamil Pek <kamil.pek@gmail.com>
2015-05-01 12:53:40 +0200

    koniec meczy

--- insert-mecze.sql
+++ insert-mecze.sql
@@ -179,3 +179,363 @@ INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
 VALUES('2013-10-05','15:00',7,16,15);
 INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
 VALUES('2013-10-05','17:00',8,12,10);
+
+-- 11. kolejka 12-13 października
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','14:00',1,10,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','14:00',2,15,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','16:00',3,3,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','14:00',4,4,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','11:00',5,13,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','14:00',6,11,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','17:00',7,2,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-12','11:00',8,14,5);
+
+-- 12. kolejka 19 października
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','14:30',1,7,5);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','14:00',2,9,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','15:00',3,6,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','15:00',4,1,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','13:00',5,8,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','14:00',6,16,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','17:00',7,12,3);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-19','14:00',8,10,15);
+
+-- 13. kolejka 26-27 października
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-26','12:00',1,15,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-26','12:00',2,3,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-26','14:00',3,4,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-26','13:00',4,13,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-26','14:00',5,11,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-26','17:00',6,2,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-27','11:00',7,14,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-10-26','13:00',8,5,9);
+
+-- 14. kolejka 2-3 listopada
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-02','14:00',1,7,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-02','14:00',2,6,5);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-03','11:00',3,1,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-02','14:00',4,8,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-02','13:00',5,16,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-02','17:00',6,12,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-02','13:00',7,10,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-03','14:30',8,15,3);
+
+-- 15. kolejka 9-10 listopada
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-09','11:00',1,3,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-09','14:00',2,4,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-09','11:00',3,13,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-09','13:00',4,11,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-09','14:30',5,2,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-10','11:00',6,14,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-09','13:00',7,5,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-09','13:00',8,9,6);
+
+-- 16. kolejka 16-17 listopada
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-16','13:00',1,7,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-16','13:00',2,9,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-16','13:00',3,5,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-16','11:00',4,14,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-17','15:00',5,2,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-16','13:00',6,11,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-16','11:00',7,13,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-16','13:00',8,4,3);
+
+-- 17. kolejka 23-24 listopada
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-23','13:00',1,4,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-23','11:00',2,3,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-23','12:00',3,15,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-23','12:00',4,10,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-23','17:00',5,12,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-23','13:00',6,16,5);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-23','13:00',7,8,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2013-11-24','17:00',8,1,6);
+
+-- 18. kolejka 15-16 marca
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-15','14:00',1,7,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-15','13:00',2,4,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-15','15:00',3,9,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-15','13:00',4,5,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-15','11:00',5,14,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-15','17:00',6,2,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-15','15:00',7,11,3);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-16','11:00',8,13,4);
+
+-- 19. kolejka 22-23 marca
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','15:00',1,13,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','15:00',2,4,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','13:00',3,3,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','14:00',4,15,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','15:00',5,10,5);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','17:30',6,12,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','15:00',7,16,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-22','15:00',8,8,1);
+
+-- 20. kolejka 29-30 marca
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-29','14:00',1,7,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-29','11:00',2,1,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-29','14:00',3,6,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-29','15:00',4,9,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-29','13:00',5,5,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-29','11:00',6,14,3);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-28','18:00',7,2,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-03-29','16:00',8,11,13);
+
+-- 21. kolejka 5-6 kwietnia
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-05','15:00',1,11,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-06','11:00',2,13,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-05','15:00',3,4,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-05','16:00',4,3,5);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-05','14:00',5,15,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-06','14:00',6,10,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-05','16:00',7,12,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-05','16:00',8,16,8);
+
+-- 22. kolejka 12-13 kwietnia
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-12','14:00',1,7,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-12','16:00',2,8,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-13','11:00',3,1,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-12','15:00',4,6,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-12','16:00',5,9,3);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-12','13:00',6,5,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-13','11:00',7,14,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-12','18:00',8,2,11);
+
+-- 23. kolejka 19 kwietnia
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','13:00',1,5,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','11:00',2,2,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','11:00',3,11,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','15:00',4,4,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','11:00',5,3,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','12:00',6,15,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','14:00',7,10,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-19','16:00',8,12,16);
+
+-- 24. kolejka 26-27 kwietnia
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-26','16:00',1,7,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-27','14:00',2,16,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-26','15:00',3,8,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-27','15:00',4,1,3);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-26','15:00',5,6,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-26','17:00',6,9,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-26','15:00',7,5,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-26','11:00',8,14,2);
+
+-- 25. kolejka 3-4 maja
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-04-30','18:00',1,14,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-03','11:00',2,2,5);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-01','15:00',3,11,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-04','17:30',4,13,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-03','15:00',5,4,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-01','16:00',6,3,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-01','11:00',7,15,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-04','11:00',8,10,12);
+
+-- 26. kolejka 10-11 maja
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-10','16:00',1,7,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-10','16:00',2,12,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-10','17:00',3,16,3);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-10','15:00',4,8,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-11','11:00',5,1,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-10','16:00',6,6,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-10','17:00',7,9,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-10','15:00',8,5,14);
+
+-- 27. kolejka 17-18 maja
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-17','13:00',1,5,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-17','11:00',2,14,9);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-17','17:30',3,2,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-18','16:00',4,11,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-17','13:00',5,13,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-17','15:00',6,4,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-17','17:00',7,2,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-17','12:00',8,15,10);
+
+-- 28. kolejka 24-25 maja
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-24','16:00',1,7,15);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-23','17:00',2,10,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-24','16:00',3,12,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-24','17:00',4,16,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-23','17:00',5,8,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-24','17:15',6,1,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-24','17:00',7,6,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-24','17:00',8,9,5);
+
+-- 29. kolejka 31 maja - 1 czerwca
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-31','17:00',1,9,7);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-31','15:00',2,5,6);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-30','16:00',3,14,1);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-30','17:00',4,2,8);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-31','15:00',5,11,16);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-01','13:00',6,13,12);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-31','15:00',7,4,10);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-05-31','15:00',8,3,15);
+
+-- 30. kolejka 7 czerwca
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',1,7,3);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',2,15,4);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',3,10,13);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',4,12,11);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',5,16,2);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',6,8,14);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',7,1,5);
+INSERT INTO mecz(data, godzina, sedzia, gospodarz, gosc)
+VALUES('2014-06-07','17:00',8,6,9);
